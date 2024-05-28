16068188325f0dcd21009a1c3e74b452809403e7
Vangelis Katsikaros <vkatsikaros@gmail.com>
2024-05-28 16:05:26 +0300

    Code: step 1

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -1,23 +1,14 @@
 import requests
 from bs4 import BeautifulSoup
 
-# Define the URL of the website you want to scrape
-url = 'https://vkatsikaros.github.io/dataharvest24-www.github.io/'
-
-# Send an HTTP GET request to the URL
+url = 'https://example-blog.com'
 response = requests.get(url)
 
-# Check if the request was successful (status code 200)
 if response.status_code == 200:
-   # Parse the HTML content of the page
-   soup = BeautifulSoup(response.content, 'html.parser')
-  
-   # Find all the article titles on the page
-   # This example assumes that the article titles are contained in <h2> tags
-   titles = soup.find_all('h2')
-  
-   # Loop through the titles and print them out
-   for title in titles:
-       print(title.get_text())
+    soup = BeautifulSoup(response.content, 'html.parser')
+    titles = soup.find_all('h2')
+    
+    for title in titles:
+        print(title.get_text())
 else:
-   print('Failed to retrieve the webpage. Status code:', response.status_code)
+    print('Failed to retrieve the webpage. Status code:', response.status_code)
